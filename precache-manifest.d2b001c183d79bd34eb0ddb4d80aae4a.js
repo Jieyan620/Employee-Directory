2a7d148336873f51be7ac139d8faea5aa85e204d
Jie Yan <jieyan620@gmail.com>
2020-03-25 20:07:53 -0700

    Updates

---
+++ precache-manifest.d2b001c183d79bd34eb0ddb4d80aae4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "491a9e0c9056ea37580d",
+    "url": "/Employee-Directory/static/css/main.ec5b68e9.chunk.css"
+  },
+  {
+    "revision": "491a9e0c9056ea37580d",
+    "url": "/Employee-Directory/static/js/main.ad03c7ff.chunk.js"
+  },
+  {
+    "revision": "1794aedb7da2e07d4918",
+    "url": "/Employee-Directory/static/js/runtime~main.51f3d348.js"
+  },
+  {
+    "revision": "3f03175b3c89cae4abd6",
+    "url": "/Employee-Directory/static/js/2.b74005f8.chunk.js"
+  },
+  {
+    "revision": "fca38c4c2cb19ccf3a89d6a227c96493",
+    "url": "/Employee-Directory/index.html"
+  }
+];
